17457e46ddc968eddb6388591dab049861bb8cae
Rye303 <2drumit@gmail.com>
2016-10-31 01:51:57 -0400

    Add files via upload

---
+++ RFID_v1.a.ino
@@ -0,0 +1,51 @@
+/*
+ * 
+ * FOR GOD SAKES DONT TOUCH THIS CODE!
+ * 
+ * 
+ * OCT 28, 2016
+ * Ryan McLean
+ * 
+ * Reads the card ID in one line
+ * 
+ * 
+ * 
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+byte readCard[4];    // Stores scanned ID read from RFID Module
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communications with the PC
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.println("Scan PICC to see UID and type...");
+}
+
+void loop() {
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  for (int i = 0; i < 4; i++) { 
+    readCard[i] = mfrc522.uid.uidByte[i];
+    Serial.print(readCard[i], HEX);
+  }
+    Serial.println("");
+  mfrc522.PICC_HaltA(); // Stop reading
+  return 1;
+  
+}
